7f0fe99e92de96a3e17e343944d243f795ca19dc
Gabriel Bispo <bispogabriel333@gmail.com>
2023-04-10 21:37:53 -0300

    initial commit

---
+++ src/br/com/abc/exceptionsloianegroner/F_criandoexcecao/UsandoMinhaException02.java
@@ -0,0 +1,31 @@
+package br.com.abc.exceptionsloianegroner.F_criandoexcecao;
+
+public class UsandoMinhaException02 {
+
+    public static void main(String[] args) {
+        try {
+            teste();
+        } catch(DivisaoNaoExata e) {
+            System.out.println("Aconteceu um erro.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void teste() throws DivisaoNaoExata {
+        int[] numeros = new int[]{4, 8, 5, 16, 21, 32, 64, 127};
+        int[] denominadores = new int[]{2, 2, 4, 0, 4, 8, 0};
+
+        for(int i = 0; i < numeros.length; i++) {
+            try {
+                if(numeros[i] % 2 != 0) {
+                    throw new DivisaoNaoExata(numeros[i], denominadores[i]);
+                }
+                System.out.println(numeros[i] + "/" + denominadores[i] +  " = " + numeros[i]/denominadores[i]);
+            } catch(ArithmeticException | ArrayIndexOutOfBoundsException e) {
+                System.out.println("Aconteceu um erro.");
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
