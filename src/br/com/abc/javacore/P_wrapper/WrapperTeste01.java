1b771e4a65f56fb83fd2f33f10444f095126ddc2
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-04-22 17:42:49 -0300

    initial commit

---
+++ src/br/com/abc/javacore/P_wrapper/WrapperTeste01.java
@@ -0,0 +1,30 @@
+package br.com.abc.javacore.P_wrapper;
+
+//Wrappers são tipos objetos que irão encapsular os tipos primitivos transformando-os em objetos,
+//possibilitando que seja feita a passagem dos valores numércos por referência e nao por valor.
+
+public class WrapperTeste01 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        char charP = 'W';
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10f;
+        double doubleP = 10d;
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1; // boxing ou autoboxing
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'W';
+        Boolean booleanW = false;
+
+        int i = intW; // unboxing
+        int i2 = Integer.parseInt("1");
+        System.out.println(i2);
+    }
+}
