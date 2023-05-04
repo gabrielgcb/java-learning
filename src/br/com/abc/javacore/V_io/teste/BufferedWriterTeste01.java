cdbcabec50accb9fba3c7aa1ab386154a15113e5
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-05-04 14:17:44 -0300

    classes utilitárias - BufferedWriter

--- src/br/com/abc/javacore/V_io/teste/BufferedWriterTeste01.java
+++ src/br/com/abc/javacore/V_io/teste/BufferedWriterTeste01.java
@@ -9,10 +9,10 @@ public class BufferedWriterTeste01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try(FileWriter fw = new FileWriter(file, true);
-            BufferedWriter br = new BufferedWriter(fw)) {
-            br.write("O Gabriel esta aprendendo Java, não desista meu parceiro. Vai dar certo. Confie em Deus!!!");
-            br.newLine();
-            br.flush();
+            BufferedWriter bw = new BufferedWriter(fw)) {
+            bw.write("O Gabriel esta aprendendo Java, não desista meu parceiro. Vai dar certo. Confie em Deus!!!");
+            bw.newLine();
+            bw.flush();
 
         } catch (IOException e) {
             e.printStackTrace();
