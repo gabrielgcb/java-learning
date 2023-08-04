c75d50fb1cba5619ca4d012fe837035692cbfba3
Gabriel Bispo <gabriel.bispo@discente.ufg.br>
2023-08-04 12:33:07 -0300

    comportamento por parametro

---
+++ src/br/com/abc/javacore/ZZBcomportamento/interfaces/CarPredicate.java
@@ -0,0 +1,7 @@
+package br.com.abc.javacore.ZZBcomportamento.interfaces;
+
+import br.com.abc.javacore.ZZBcomportamento.dominio.Car;
+
+public interface CarPredicate {
+    boolean test(Car car);
+}
---
+++ src/br/com/abc/javacore/ZZBcomportamento/teste/ComportamentoPorParametroTeste02.java
@@ -0,0 +1,61 @@
+package br.com.abc.javacore.ZZBcomportamento.teste;
+
+import br.com.abc.javacore.ZZBcomportamento.dominio.Car;
+import br.com.abc.javacore.ZZBcomportamento.interfaces.CarPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComportamentoPorParametroTeste02 {
+
+    static List<Car> cars = List.of(
+            new Car("green", 2015),
+            new Car("black", 2022),
+            new Car("white", 2010),
+            new Car("white", 2005),
+            new Car("black", 1994)
+    );
+
+    public static void main(String[] args) {
+
+        List<Car> greenCars = filter(cars, new CarPredicate() {
+            @Override
+            public boolean test(Car car) {
+                return car.getColor().equals("green");
+            }
+        });
+
+        System.out.println("green cars: " + greenCars);
+        System.out.println("-----------");
+
+        List<Car> blackCars = filter(cars, new CarPredicate() {
+            @Override
+            public boolean test(Car car) {
+                return car.getColor().equals("black");
+            }
+        });
+
+        System.out.println("black cars: " + blackCars);
+        System.out.println("-----------");
+
+        List<Car> carsBefore2010 = filter(cars, new CarPredicate() {
+            @Override
+            public boolean test(Car car) {
+                return car.getYear() < 2010;
+            }
+        });
+
+        System.out.println("cars before 2010: " + carsBefore2010);
+
+    }
+
+    public static List<Car> filter(List<Car> carList, CarPredicate carPredicate) {
+        List<Car> filteredCars = new ArrayList<>();
+        for (Car car : carList) {
+            if (carPredicate.test(car)) {
+                filteredCars.add(car);
+            }
+        }
+        return filteredCars;
+    }
+}
